e9ad4f620b2bf7795dd1a81ba40bcdb04778ba6a
Aluno <Aluno@sn633>
2019-11-29 22:51:44 -0200

    clima-tempo

--- index.php
+++ index.php
@@ -1,14 +1,51 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Projeto Climar</title>
+        <title>Projeto clima-tempo</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="js/clima.js"></script>
+
+        <!-- Tags Gerais -->
+        <meta NAME="DESCRIPTION" CONTENT="Site para se manter informado sobre o tempo de Brusque/Desenvolvido por Lucas kempner"/>
+        <meta NAME="ABSTRACT" CONTENT="Informações climaticas"/>
+        <meta name="keywords" content="clima, temperatura, temperatura de Brusque, tempo nesse momento em brusque, tempo maximo,tempo minimo, tempo brusque santa catarina, 
+              tempo hoje, umidade brusque,lucas kempner,
+              clima hoje,clima tempo,tempo nesse momento,projeto clima desenvolvido por lucas,
+              tempo ruim em brusque,tempo para o fim de semana,fim de semana chuvoso em brusque,sol nesse final de semana em brusque"/>
+        <meta NAME="title" CONTENT="Projeto clima-tempo" />
+        <meta NAME="identifier-url" content="https://lucas-clima-senai.herokuapp.com" />
+        <meta NAME="author" content="Lucas Kempner" />
+        <meta NAME="ROBOTS" CONTENT="All" />
+        <meta NAME="RATING" CONTENT="general" />
+        <meta NAME="DISTRIBUTION" CONTENT="global" />
+        <meta NAME="LANGUAGE" CONTENT="pt-br" />
+        <meta name="content-language" content="portuguese" />
+        <meta name="doc-class" content="Completed" /> 
+        <meta name="reply-to" content="lucas_kempner@estudante.sc.senai.br"/>
+
+        <!-- Tags Para rede social Facebook -->
+        <meta property="og:url" content="https://lucas-clima-senai.herokuapp.com" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="Site para se manter informado sobre o tempo de Brusque/Desenvolvido por Lucas kempner" />
+        <meta property="og:image" content="https://lucas-clima-senai.herokuapp.com/img/icones/por-do-sol.png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="120" />
+        <meta property="fb:app_id" content="" />
+
+        <!-- Tags Para rede social Twitter -->
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Site para se manter informado sobre o tempo de Brusque/Desenvolvido por Lucas kempner" />
+        <meta name="twitter:title" content="Projeto clima-tempo" />
+
     </head>
+
     <body>
         <div class="alert alert-danger mt-3">
             <button type="button" class="close" data-dismiss="alert">&times;</button>
@@ -63,8 +100,8 @@
                         <span id="situacao"></span>
                         <img src="img/icones/por-do-sol.png" width="100px">
                 </div>
-                
-                
+
+
                 <div class="col-md-6">
                     <h2>Anoitecer| <span id="Anoitecer"></span></h2>
                     <p>
